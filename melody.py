4731adac24894238e60d9e5ae5d89cccffb178f2
Kosciany <karol.kosciany@gmail.com>
2018-12-18 13:46:56 +0100

    software pwm to hardware pwm

--- melody.py
+++ melody.py
@@ -1,7 +1,7 @@
 import pigpio
 import time
 
-tempo = 1
+tempo = 2
 cala = tempo
 pol = tempo/2
 cwierc = tempo/4
@@ -18,27 +18,28 @@ C_2 = 523
 
 
 class PiTone:
-  buzzer = 24 #pin 12 pinem silnika
   def __init__(self):
     self.pi = pigpio.pi()
-  def spiewaj(nuty,czas):
+  def spiewaj(self,nuty,czasy):
+    buzzer = 18 #na tym pinie może być hardware PWM, pin 6 na drugim paśmie
     if len(nuty) is not len(czasy):
       return -1
     for i in range (0,len(nuty)):
-      self.pi.set_PWM_dutycycle(buzzer,nuty[i])
-      time.sleep(czasy[i]-0.1)
-      self.pi.set_PWM_dutycycle(buzzer,0)
-      time.sleep(0.1)
-    self.pi.set_PWM_dutycycle(buzzer,0)
+      self.pi.hardware_PWM(buzzer, nuty[i], 500000) # nuty[i]Hz 50% dutycycle
+      print(nuty[i])
+      time.sleep(0.9*czasy[i])
+      self.pi.hardware_PWM(buzzer, nuty[i], 0)
+      time.sleep(0.1*czasy[i])
+    self.pi.hardware_PWM(buzzer, 0, 0)
     
     
     
-sto_lat_nuty =[G,E,G,E,G,A,G,F,E,F
-               F,D,F,D,F,G,F,E,D,E
-               G,G,E,G,G,E,G,C,H,A,G,A
-               H,H,H,C]
+sto_lat_nuty =[G,E,G,E,G,A,G,F,E,F,
+               F,D,F,D,F,G,F,E,D,E,
+               G,G,E,G,G,E,G,C_2,H,A,G,A,
+               H,H,H,C_2]
 sto_lat_czas =[pol, pol, pol, pol, pol, cwierc, cwierc, cwierc, cwierc, pol,
-               pol, pol, pol, pol, pol, cwierc, cwierc, cwierc, cwierc, pol
+               pol, pol, pol, pol, pol, cwierc, cwierc, cwierc, cwierc, pol,
                cwierc,cwierc, pol, cwierc, cwierc, pol, pol, cwierc, cwierc, cwierc, cwierc, pol,
                cala, pol, pol, cala]
                
