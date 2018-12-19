1f79b9140717a4ed4d076f0baada928b1bbcaaf6
Kosciany <karol.kosciany@gmail.com>
2018-12-19 15:20:32 +0100

    edited blink

--- blink.py
+++ blink.py
@@ -4,11 +4,15 @@ from time import sleep
 pi = pigpio.pi()     
 
 led1 = 10
-led2 = 11
-led3 = 13
-led4 = 14
+led2 = 9
+led3 = 11
+led4 = 22
+read_pin = 5
 
 while True:
+  while True:
+      if pi.read(read_pin) is 1:
+          break
   pi.write(led1, 1) 
   pi.write(led2, 1) 
   sleep(1)
@@ -19,3 +23,4 @@ while True:
   pi.write(led2,0)
   pi.write(led3,0)
   pi.write(led4,0)
+  sleep(1)
